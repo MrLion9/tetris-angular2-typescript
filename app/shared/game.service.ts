3e57459038e07a165bc8cef80ae850c80e36dca9
WorkFin <finthehuman9@gmail.com>
2016-08-24 12:30:27 +0300

    add start method, reset detail, when it reaches bottom of the board

--- app/shared/game.service.ts
+++ app/shared/game.service.ts
@@ -4,6 +4,10 @@ import * as _ from 'lodash';
 
 @Injectable()
 export class GameService{
+    isStarted: boolean;
+    isGameOver: boolean;
+    score: number;
+    interval: number;
     board: string[][];
     activeDetail: {};
     activePoint: number[];
@@ -12,6 +16,50 @@ export class GameService{
         this.setupBoard();
 
         this.resetDetail();
+
+        window.addEventListener('keyup', (e: any) => {
+            switch (e.keyCode) {
+                case KEYS.ENTER:
+                    this.toggle();
+                    break;
+            }
+        });
+    }
+
+    start(){
+        this.isStarted = true;
+        this.isGameOver = false;
+        this.score = 0;
+        this.interval = 1000;
+
+        this.update();
+    }
+
+    toggle() {
+        if (this.isStarted) {
+            this.gameOver();
+        } else {
+            this.start();
+        }
+    }
+
+    update(){
+        let self: GameService = this;
+        if (this.isStarted) {
+            setTimeout(() => {
+                if(self.moveDownDetail()){
+                    self.activePoint[0] = self.activePoint[0] + 1;
+                }else{
+                    self.resetDetail();
+                }
+
+                self.update();
+            }, self.interval);
+        }
+    }
+
+    gameOver(){
+
     }
 
     setupBoard(){
@@ -24,14 +72,30 @@ export class GameService{
         }
     }
 
-    resetDetail(){
-        this.activeDetail = Details[_.random(0, 4)];
+    moveDownDetail(){
+        let pos = this.activeDetail['positions'][this.activeDetail['actualPosition'] ];
+        for(let i = 0; i < pos.length; i++){
+            for (let j = 0; j < pos[i].length; j++){
+                if(pos[i][j] == 1) {
+                    let y = this.activePoint[0] + i + 1;
+                    let x = this.activePoint[1] + j;
+                    if( y > (BOARD_SIZE - 1) || this.board[y][x] != "empty") return false;
+                }
+            }
+        }
+        return true;
+    }
 
-        this.activePoint = start_point;
+    resetDetail(){
+
+        this.activeDetail = Details[_.random(0, 4)];
+        this.activeDetail['actualPosition'] = 0;
+
+        this.activePoint = _.clone(start_point);
     }
 
     setDetailPart(y: number, x: number){
-        let pos = this.activeDetail['positions'][0];
+        let pos = this.activeDetail['positions'][this.activeDetail['actualPosition'] ];
         for(let i = 0; i < pos.length; i++){
             for (let j = 0; j < pos[i].length; j++){
                 if(pos[i][j] == 1) {
